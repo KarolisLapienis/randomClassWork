8f0b3345add242fa91b2e52237a39e834453751b
gedasma@gmail.com <gedasma@gmail.com>
2023-11-08 12:59:11 +0200

    initial commit of my work in class from pc 11

---
+++ javascriptPirmas/pirmasJS.js
@@ -0,0 +1,24 @@
+//"use strict";
+
+let x = 1;
+var y = 2;
+
+var name1 = "Petras";  // var nenaudoti!!! naudoti let arba const
+
+let name2 = "Jonas";
+
+const nameConst = "Algis";
+
+let name3;
+
+name4 = "LALALA"
+
+name3 = "Kazimieras";
+
+
+console.log(x, y, name1, name2, nameConst);
+
+console.log(name3);
+
+console.log(name4)
+
