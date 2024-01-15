e5db658f0abc84fb5eed85a8c6aa1370f679dd7a
KaLa <k.lapienis@gmail.com>
2024-01-15 16:18:08 +0200

    po pirmadienio firebase integration

---
+++ firebaseIntegration/firebaseIntegration.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2eq8qf9ZoCRWZUI_QQhyG7j0iklRa4mQ",
+  authDomain: "fir-integration-54e22.firebaseapp.com",
+  projectId: "fir-integration-54e22",
+  storageBucket: "fir-integration-54e22.appspot.com",
+  messagingSenderId: "720443876417",
+  appId: "1:720443876417:web:5b3f7a133e1c37c5c892c0",
+  databaseURL: "https://fir-integration-54e22-default-rtdb.europe-west1.firebasedatabase.app"
+};
